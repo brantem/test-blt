97401e10c6be2009621250121146265a8075171e
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2025-01-25 23:28:46 +0700

    Add ws endpoint at /api

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import url from 'node:url';
 import next from 'next';
+import WebSocket, { WebSocketServer } from 'ws';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
@@ -11,6 +12,72 @@ app.prepare().then(async () => {
   const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url!, true);
     handle(req, res, parsedUrl);
+  });
+
+  // store rooms and their connected clients
+  const rooms = new Map<number, Set<WebSocket>>();
+
+  // 1. noServer is set to true to handle the upgrade manually and avoid interfering with next ws server (hmr)
+  // 2. path is set to /api to prevent immediate disconnection after connecting
+  const wss = new WebSocketServer({ noServer: true, path: '/api' });
+  wss.on('connection', async (ws, req) => {
+    console.log(`Client connected`);
+
+    // get roomId and userId
+    const url = new URL(req.url || '', `http://${req.headers.host}`);
+    const roomId = Number(url.searchParams.get('roomId'));
+    const userId = Number(url.searchParams.get('userId'));
+
+    if (!roomId || !userId) {
+      console.log('Missing roomId or userId. Closing connection.');
+      return ws.close();
+    }
+
+    // add client
+    if (!rooms.has(roomId)) rooms.set(roomId, new Set());
+    rooms.get(roomId)!.add(ws);
+
+    // TODO: get and send previous messages
+
+    ws.on('message', async (v) => {
+      const { type, data } = JSON.parse(v.toString());
+      switch (type) {
+        case 'message':
+          // TODO: save into db
+
+          const clients = rooms.get(roomId);
+          if (!clients) return;
+
+          clients.forEach((client) => {
+            if (client.readyState !== WebSocket.OPEN) return;
+
+            // send new message
+            const message = { id: Date.now(), userId, content: data, createdAt: Date.now() };
+            client.send(JSON.stringify({ type: 'new_message', data: message }));
+          });
+          break;
+      }
+    });
+
+    ws.on('close', () => {
+      console.log('Client disconnected');
+
+      const clients = rooms.get(roomId);
+      if (!clients) return;
+      clients.delete(ws);
+
+      if (clients.size !== 0) return;
+      rooms.delete(roomId);
+    });
+  });
+
+  // handle ws upgrade
+  server.on('upgrade', function (req, socket, head) {
+    const { pathname } = url.parse(req.url!, true);
+    if (pathname === '/_next/webpack-hmr') return;
+    wss.handleUpgrade(req, socket, head, function done(ws) {
+      wss.emit('connection', ws, req);
+    });
   });
 
   server.listen(port);
